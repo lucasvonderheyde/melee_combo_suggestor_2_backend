7b59de138c499c5634d7ccbafa6bda9b7c7acc65
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2024-02-23 08:12:01 -0500

    setting up other important local environment variables

--- data_processing/slippi.js
+++ data_processing/slippi.js
@@ -4,13 +4,15 @@ const game = new SlippiGame("test.slp");
 
 // Get game settings – stage, characters, etc
 const settings = game.getSettings();
-console.log(settings);
+// console.log(settings);
 
 // Get metadata - start time, platform played on, etc
 const metadata = game.getMetadata();
-console.log(metadata);
+lastFrame = metadata.lastFrame
+console.log(typeof(metadata))
+
 
 // Get frames – animation state, inputs, etc
 // This is used to compute your own stats or get more frame-specific info (advanced)
 const frames = game.getFrames();
-console.log(frames[0].players); // Print frame when timer starts counting down
+// console.log(frames[0].players); // Print frame when timer starts counting down
