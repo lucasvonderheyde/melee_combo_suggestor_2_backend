dfaaf64245113e7484db6fe2bd4295085d87a138
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2024-02-26 22:11:58 -0500

    settings json being properly split and generated

--- data_processing/slippi.js
+++ data_processing/slippi.js
@@ -1,15 +1,43 @@
-const { SlippiGame } = require("@slippi/slippi-js");
 
-const game = new SlippiGame("test.slp");
 
-// Get game settings – stage, characters, etc
-const settings = game.getSettings();
-// console.log(settings);
+const { SlippiGame } = require("@slippi/slippi-js")
+const fs = require('fs')
+const uuid = require('uuid')
 
-// Get metadata - start time, platform played on, etc
-const metadata = game.getMetadata();
+const game = new SlippiGame("test.slp")
+
+gameUuid = uuid.v4()
+
+const settings = game.getSettings()
+const players = settings.players
+const gameInfo = settings.gameInfoBlock
+const matchInfo = settings.matchInfo
+
+delete settings.players 
+delete settings.gameInfoBlock
+delete settings.matchInfo
+
+const settingsFilename = `settings_${gameUuid}.json`
+const playersFilename = `players_${gameUuid}.json`
+const gameInfoBlockFilename = `gameInfoBlock_${gameUuid}.json`
+const matchInfoFilename = `matchInfo_${gameUuid}.json`
+
+function writeToJson(filename, data) {
+    fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
+        if (err) throw err;
+        console.log(`${filename} has been saved.`)
+    });
+}
+
+writeToJson(settingsFilename, settings)
+writeToJson(playersFilename, players)
+writeToJson(gameInfoBlockFilename, gameInfo)
+writeToJson(matchInfoFilename, matchInfo)
+
+
+const metadata = game.getMetadata()
 lastFrame = metadata.lastFrame
-console.log(typeof(metadata))
+// console.log(typeof(metadata))
 
 
 // Get frames – animation state, inputs, etc
