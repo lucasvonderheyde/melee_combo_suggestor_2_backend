6223d188244989db35e2bb3b51d1286ab48e4b6a
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2024-02-27 20:42:49 -0500

    json for metadata being pulled and written properly

--- data_processing/slippi.js
+++ data_processing/slippi.js
@@ -8,20 +8,6 @@ const game = new SlippiGame("test.slp")
 
 gameUuid = uuid.v4()
 
-const settings = game.getSettings()
-const players = settings.players
-const gameInfo = settings.gameInfoBlock
-const matchInfo = settings.matchInfo
-
-delete settings.players 
-delete settings.gameInfoBlock
-delete settings.matchInfo
-
-const settingsFilename = `settings_${gameUuid}.json`
-const playersFilename = `players_${gameUuid}.json`
-const gameInfoBlockFilename = `gameInfoBlock_${gameUuid}.json`
-const matchInfoFilename = `matchInfo_${gameUuid}.json`
-
 function writeToJson(filename, data) {
     fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
         if (err) throw err;
@@ -29,15 +15,50 @@ function writeToJson(filename, data) {
     });
 }
 
-writeToJson(settingsFilename, settings)
-writeToJson(playersFilename, players)
-writeToJson(gameInfoBlockFilename, gameInfo)
-writeToJson(matchInfoFilename, matchInfo)
-
-
+const settings = game.getSettings()
 const metadata = game.getMetadata()
-lastFrame = metadata.lastFrame
-// console.log(typeof(metadata))
+
+
+const metadataPlayers = metadata.players
+const lowerportMetadataNames = metadataPlayers[0].names
+const lowerportMetadataCharacters = metadataPlayers[0].characters
+const higherportMetadataNames = metadataPlayers[1].names
+const higherportMetadataCharacters = metadataPlayers[1].characters
+const lastFrame = metadata.lastFrame
+
+const players = settings.players
+const gameInfo = settings.gameInfoBlock
+const matchInfo = settings.matchInfo
+
+delete metadata.players
+
+delete settings.players 
+delete settings.gameInfoBlock
+delete settings.matchInfo
+
+const metadataFilename = `metadata_${gameUuid}.json`
+const lowerportMetadataCharactersFilename = `lowerportMetadataCharacters_${gameUuid}.json`
+const lowerportMetadataNamesFilename = `lowerportMetadataNames_${gameUuid}.json`
+const higherportMetadataCharactersFilename = `higherportMetadataCharacters_${gameUuid}.json`
+const higherportMetadataNamesFilename = `higherportMetadataNames_${gameUuid}.json`
+
+
+const settingsFilename = `settings_${gameUuid}.json`
+const playersFilename = `players_${gameUuid}.json`
+const gameInfoBlockFilename = `gameInfoBlock_${gameUuid}.json`
+const matchInfoFilename = `matchInfo_${gameUuid}.json`
+
+// writeToJson(metadataFilename, metadata)
+writeToJson(lowerportMetadataCharactersFilename, lowerportMetadataCharacters)
+writeToJson(lowerportMetadataNamesFilename, lowerportMetadataNames)
+writeToJson(higherportMetadataCharactersFilename, higherportMetadataCharacters)
+writeToJson(higherportMetadataNamesFilename, higherportMetadataNames)
+
+
+// writeToJson(settingsFilename, settings)
+// writeToJson(playersFilename, players)
+// writeToJson(gameInfoBlockFilename, gameInfo)
+// writeToJson(matchInfoFilename, matchInfo)
 
 
 // Get frames – animation state, inputs, etc
