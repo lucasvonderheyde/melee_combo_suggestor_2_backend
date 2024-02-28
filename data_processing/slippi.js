ed3dfe89f311d97af6896436635882fa376bc444
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2024-02-27 22:22:52 -0500

    all jsons being properly written

--- data_processing/slippi.js
+++ data_processing/slippi.js
@@ -1,5 +1,3 @@
-
-
 const { SlippiGame } = require("@slippi/slippi-js")
 const fs = require('fs')
 const uuid = require('uuid')
@@ -19,14 +17,12 @@ const settings = game.getSettings()
 const metadata = game.getMetadata()
 const frames = game.getFrames();
 
-
 const metadataPlayers = metadata.players
 const lowerportMetadataNames = metadataPlayers[0].names
 const lowerportMetadataCharacters = metadataPlayers[0].characters
 const higherportMetadataNames = metadataPlayers[1].names
 const higherportMetadataCharacters = metadataPlayers[1].characters
 const lastFrame = metadata.lastFrame
-console.log(lastFrame)
 
 const players = settings.players
 const gameInfo = settings.gameInfoBlock
@@ -45,40 +41,43 @@ const higherportPlayerPostFrames = []
 
 let frameIndex = 0
 
-console.log(frames[0].players[0].pre)
-
 while (frameIndex <= lastFrame) {
+
     lowerportPlayerPreFrames.push(frames[frameIndex].players[0].pre)
     lowerportPlayerPostFrames.push(frames[frameIndex].players[0].post)
     higherportPlayerPreFrames.push(frames[frameIndex].players[1].pre)
     higherportPlayerPostFrames.push(frames[frameIndex].players[1].post)
+
     frameIndex ++
 }
-
 
 const metadataFilename = `metadata_${gameUuid}.json`
 const lowerportMetadataCharactersFilename = `lowerportMetadataCharacters_${gameUuid}.json`
 const lowerportMetadataNamesFilename = `lowerportMetadataNames_${gameUuid}.json`
 const higherportMetadataCharactersFilename = `higherportMetadataCharacters_${gameUuid}.json`
 const higherportMetadataNamesFilename = `higherportMetadataNames_${gameUuid}.json`
-
-
+const lowerportPlayerPreFramesFilename = `lowerportPreFrames_${gameUuid}.json`
+const lowerportPlayerPostFramesFilename = `lowerportPostFrames_${gameUuid}.json`
+const higherportPlayerPreFramesFilename = `higherportPreFrames_${gameUuid}.json`
+const higherportPlayerPostFramesFilename = `higherportPostFrames_${gameUuid}.json`
 const settingsFilename = `settings_${gameUuid}.json`
 const playersFilename = `players_${gameUuid}.json`
 const gameInfoBlockFilename = `gameInfoBlock_${gameUuid}.json`
 const matchInfoFilename = `matchInfo_${gameUuid}.json`
 
 
-// writeToJson(metadataFilename, metadata)
-// writeToJson(lowerportMetadataCharactersFilename, lowerportMetadataCharacters)
-// writeToJson(lowerportMetadataNamesFilename, lowerportMetadataNames)
-// writeToJson(higherportMetadataCharactersFilename, higherportMetadataCharacters)
-// writeToJson(higherportMetadataNamesFilename, higherportMetadataNames)
-
-
-// writeToJson(settingsFilename, settings)
-// writeToJson(playersFilename, players)
-// writeToJson(gameInfoBlockFilename, gameInfo)
-// writeToJson(matchInfoFilename, matchInfo)
+writeToJson(metadataFilename, metadata)
+writeToJson(lowerportMetadataCharactersFilename, lowerportMetadataCharacters)
+writeToJson(lowerportMetadataNamesFilename, lowerportMetadataNames)
+writeToJson(higherportMetadataCharactersFilename, higherportMetadataCharacters)
+writeToJson(higherportMetadataNamesFilename, higherportMetadataNames)
+writeToJson(settingsFilename, settings)
+writeToJson(playersFilename, players)
+writeToJson(gameInfoBlockFilename, gameInfo)
+writeToJson(matchInfoFilename, matchInfo)
+writeToJson(lowerportPlayerPreFramesFilename, lowerportPlayerPreFrames)
+writeToJson(lowerportPlayerPostFramesFilename, lowerportPlayerPostFrames)
+writeToJson(higherportPlayerPreFramesFilename, higherportPlayerPreFrames)
+writeToJson(higherportPlayerPostFramesFilename, higherportPlayerPostFrames)
 
 // console.log(frames[0].players);
